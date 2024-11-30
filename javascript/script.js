7fe9f165091f40c4509f51efddd1df0cb9141244
kalouleloup <lucaspalu866@gmail.com>
2024-12-01 00:56:56 +0100

    work oh home page

--- javascript/script.js
+++ javascript/script.js
@@ -1,28 +1,28 @@
 // Button for links inscirption
-const ctaButton = document.getElementById("cta-button");
 const overlay = document.querySelector(".overlay");
 const menu = document.querySelector("#menu");
 const links = document.querySelector(".links");
 
-// Fonction pour afficher le menu diagonal
-ctaButton.addEventListener("click", () => {
-  menu.classList.add("four"); // Ajoute la classe pour afficher le menu diagonal
+const handleClick = (buttonId) => {
+  menu.classList = [];
+  menu.classList.add(buttonId);
   setTimeout(() => {
-    document.body.classList.add("menu-active"); // Affiche l'overlay et le menu
+    document.body.classList.add("menu-active");
   }, 100);
-});
+};
 
-// Événements pour fermer le menu en cliquant sur l'overlay ou sur les liens
-overlay.addEventListener("click", () => {
+menu.addEventListener("click", () => {
   document.body.classList.remove("menu-active");
+
   setTimeout(() => {
-    menu.classList.remove("four"); // Enlève la classe pour revenir à l'état initial
+    menu.classList = [];
   }, 400);
 });
 
 links.addEventListener("click", () => {
   document.body.classList.remove("menu-active");
+
   setTimeout(() => {
-    menu.classList.remove("four"); // Enlève la classe pour revenir à l'état initial
+    menu.classList = [];
   }, 400);
 });
