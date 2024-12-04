ea86b41465825ad1a81e79f0299886542799729c
kalouleloup <lucaspalu866@gmail.com>
2024-12-04 22:29:50 +0100

    finish all the main page + responsivness

--- javascript/script.js
+++ javascript/script.js
@@ -26,3 +26,19 @@ links.addEventListener("click", () => {
     menu.classList = [];
   }, 400);
 });
+
+// place the row table of our team in center
+document.addEventListener("DOMContentLoaded", function () {
+  // Sélectionner la ligne avec la classe 'our-team'
+  var ourTeamRow = document.querySelector(".our-team");
+
+  if (ourTeamRow) {
+    // Calculer la position de la ligne dans le tableau
+    var container = document.querySelector(".classement-scroll-container"); // Le conteneur défilant
+    var rowOffset = ourTeamRow.offsetTop; // Position de la ligne dans le tableau
+    var containerHeight = container.offsetHeight; // Hauteur du conteneur visible
+
+    // Faire défiler le tableau pour centrer la ligne avec un léger ajustement vers le haut
+    container.scrollTop = rowOffset - containerHeight / 3; // Défilement vers le haut (ici, vous pouvez ajuster le /3 pour plus ou moins de centrage)
+  }
+});
